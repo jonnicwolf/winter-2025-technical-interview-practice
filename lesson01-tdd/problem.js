ca6d986d87a7163322f00fd29b9b4ef54fa95801
Jon Narine <jon.wolf.narine@gmail.com>
2025-01-22 12:22:51 -0500

    Solves and passes lesson01 & lesson02

--- lesson01-tdd/problem.js
+++ lesson01-tdd/problem.js
@@ -11,7 +11,27 @@
  * @returns {boolean} - True if the string is a palindrome, false otherwise.
  */
 function isPalindrome(str) {
-  // your code here
+  if (str.split('').reverse().join('') === str) return true;
+
+  str = str.toLowerCase();
+  const result = [];
+
+  for (let i=0; i<str.length; i++) {
+    const a_code = 97;
+    const z_code = 122;
+    
+    if (str[i].charCodeAt() >= a_code &&
+        str[i].charCodeAt() <= z_code
+    )
+    {
+      result.push(str[i]);
+    }
+  }
+
+  const test_1 = result.join('');
+  const test_2 = result.reverse().join('');
+
+  return test_1 === test_2;
 }
 
 module.exports = isPalindrome;
--- lesson02-strings/problem.js
+++ lesson02-strings/problem.js
@@ -5,7 +5,26 @@
  * @return {number}
  */
 function lengthOfLongestSubstring(s) {
-  // Your code here
+  if (!s.length) return 0;
+  let seen = {};
+  let count = 0;
+  const results = [];
+
+  for (let i=0; i<s.length; i++) {
+    const val = s[i];
+
+    if (seen[val]) {
+      results.push(count);
+      count = 0;
+      seen = {};
+    } else {
+        count++;
+        seen[val] = val;
+
+        if (i === s.length-1) results.push(count);
+      }
+  }
+  return results.sort((a,b) => b - a)[0];
 }
 
 module.exports = lengthOfLongestSubstring;
