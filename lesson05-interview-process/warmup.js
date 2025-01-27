a42ecf46798f487e813acaf6f86d24750eb89bfb
Jon Narine <jon.wolf.narine@gmail.com>
2025-01-27 13:13:20 -0500

    Solves lesson05

--- lesson05-interview-process/warmup.js
+++ lesson05-interview-process/warmup.js
@@ -1,9 +1,27 @@
-function reverseString(str) {}
+function reverseString(str) {
+  let result = ''
+  for (let i=str.length-1; i>0; i--) {
+    result+=str[i]
+  }
+  return result;
+};
 
 console.log(reverseString("Hello, World!"));
 // Expected Output: "!dlroW ,olleH"
 
-function capitalizeWords(str) {}
+function capitalizeWords(str) {
+  str = str.split(' ');
+  const result = [];
+
+  for (val of str) {
+    let ans = ''
+    ans+=val[0].toUpperCase()
+    val = val.slice(1)
+    result.push(ans+val)
+  };
+
+  return result.join(' ');
+}
 
 console.log(capitalizeWords("hello, world!"));
 // Expected Output: 'Hello, World!'
@@ -11,7 +29,20 @@ console.log(capitalizeWords("hello, world!"));
 console.log(capitalizeWords("javascript is awesome"));
 // Expected Output: 'Javascript Is Awesome'
 
-function countVowels(str) {}
+function countVowels(str) {
+  const vowels = {
+    a: true,
+    e: true,
+    i: true,
+    o: true,
+    u: true,
+  };
+  let result = 0;
+
+  for (val of str) if (vowels[val]) result++ 
+
+  return result
+}
 
 console.log(countVowels("hello"));
 // Expected Output: 2
