901bf112b1ae92768e8d628efb32ed273c967268
Jon Narine <jon.wolf.narine@gmail.com>
2025-03-05 12:01:31 -0500

    solves lesson22 warmup and problem

--- lesson22-sliding-window/problem.js
+++ lesson22-sliding-window/problem.js
@@ -3,12 +3,38 @@
 
 // Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
 
-// console.log(maxSubarraySum([100,200,300,400], 2)) // 700
-// console.log(maxSubarraySum([1,4,2,10,23,3,1,0,20], 4))  // 39
-// console.log(maxSubarraySum([-3,4,0,-2,6,-1], 2)) // 5
-// console.log(maxSubarraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
-// console.log(maxSubarraySum([2,3], 3)) // null
+function prefixSum (arr) {
+  const prefix = [0]; //pushing to arr is faster than updating int
+  for (let i=0; i<arr.length; i++){
+    prefix.push(prefix[i] + arr[i]) //cumulative sum of each index
+  }
 
-module.exports = function maxSubArraySum(arr, subArrLength) {
+  return prefix
+}
+
+function getWindowSum (prefix, l, r) {
+  return prefix[r+1] - prefix[l] // use prefix sums to do simple subtraction instead of running over the arr each time
+}
+
+function maxSubArraySum(arr, subArrLength) {
+  const prefix = prefixSum(arr)
+  let max = -Infinity; // cover negatives
+
+  for (let i=0; i<=arr.length - subArrLength; i++) {
+    const windowSum = getWindowSum(prefix, i, i + subArrLength - 1) // add i to subArrLength and minus 1 to be inclusive
+    max = Math.max(max, windowSum)
+  }
+
+  return max !== -Infinity ? max : null; // check that k doesn't overun arr length
+};
+
+console.log(maxSubArraySum([100,200,300,400], 2)) // 700
+console.log(maxSubArraySum([1,4,2,10,23,3,1,0,20], 4))  // 39
+console.log(maxSubArraySum([-3,4,0,-2,6,-1], 2)) // 5
+console.log(maxSubArraySum([3,-2,7,-4,1,-1,4,-2,1],2)) // 5
+console.log(maxSubArraySum([2,3], 3)) // null
+
+module.exports = function maxsubArraySum(arr, subArrLength) {
   // your code here
 };
+
--- lesson22-sliding-window/warmup.js
+++ lesson22-sliding-window/warmup.js
@@ -2,3 +2,31 @@
 
 // 2. Given an array of integers return the largest sum for two consecutive numbers
 // input: [20,17,25] output: 42
+
+function prefixSum (arr) {
+  const prefix = [0]
+  for (let i = 0; i < arr.length; i++) {
+    prefix.push(prefix[i] + arr[i])
+  }
+  return prefix
+}
+
+function getWindowSum (prefix, l, r) {
+  return prefix[r+1] - prefix[l]
+}
+
+function findLargestSum (arr) {
+  const prefix = prefixSum(arr)
+  let max = -Infinity;
+
+  for (let i=0; i<arr.length-1; i++){
+    const sum = getWindowSum(prefix, i, i+1)
+    max = Math.max(sum, max)
+  }
+
+  return max;
+}
+
+const arr = [1,3,0,7]
+
+console.log(findLargestSum(arr))
